4af9058aae1b9104a0e5fc985a6e0689bcc3b80d
Manuel Quiñones <manuel.por.aca@gmail.com>
2023-09-13 11:05:20 -0300

    Rename welcomeScreen to loadingScreen
    And start Kolibri right ahead.
    
    Also remove unused imports.

--- kolibri-electron/src/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('WelcomeWrapper', {
-    // Triggers the Kolibri load, configured to use specified package
-    startWithNetwork: (packId) => {
-        ipcRenderer.send('load', { pack: packId });
-    },
-});
